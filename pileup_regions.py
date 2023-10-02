2cb70ed07047634dcfb87543705a8383a39fb81f
Mariusz Dabrowski <mgd4@poczta.onet.pl>
2023-10-02 21:03:49 +0200

    Refactoring

--- pileup_regions.py
+++ pileup_regions.py
@@ -18,21 +18,21 @@ ITEM_SEGMENT_BEGIN_NAMES = 2
 ITEM_SEGMENT_END = 3
 ITEM_SEGMENT_END_NAMES = 4
 
-def init_locus(segments, locus):
-    if (not locus in segments):
-        segments[locus] = [0, 0, [], 0, []]
+def init_location(locations, bp):
+    if (not bp in locations):
+        locations[bp] = [0, 0, [], 0, []]
 
-def register_segment_begin(segments, locus, match_name):    
-    init_locus(segments, locus)
-    segments[locus][ITEM_SEGMENT_BEGIN] += 1
-    segments[locus][ITEM_SEGMENT_BEGIN_NAMES].append(match_name)
+def register_segment_begin(locations, bp, match_name):    
+    init_location(locations, bp)
+    locations[bp][ITEM_SEGMENT_BEGIN] += 1
+    locations[bp][ITEM_SEGMENT_BEGIN_NAMES].append(match_name)
 
-def register_segment_end(segments, locus, match_name):    
-    init_locus(segments, locus)
-    segments[locus][ITEM_SEGMENT_END] += 1
-    segments[locus][ITEM_SEGMENT_END_NAMES].append(match_name)
+def register_segment_end(locations, bp, match_name):    
+    init_location(locations, bp)
+    locations[bp][ITEM_SEGMENT_END] += 1
+    locations[bp][ITEM_SEGMENT_END_NAMES].append(match_name)
 
-def register_segment(row, required_chromosome, cm_limit, segments):
+def register_segment(row, required_chromosome, cm_limit, locations):
     if (len(row) != NUMBER_OF_COLUMNS):
         return
     match_name = row[COLUMN_MATCH_NAME]
@@ -44,15 +44,15 @@ def register_segment(row, required_chromosome, cm_limit, segments):
         return
     if (centimorgans < cm_limit):
         return
-    register_segment_begin(segments, loc_beg, match_name)    
-    register_segment_end(segments, loc_end, match_name)
+    register_segment_begin(locations, loc_beg, match_name)    
+    register_segment_end(locations, loc_end, match_name)
 
-def calculate_number_of_matches(segments):
+def calculate_number_of_matches(locations):
     number_of_matches_after_locus = 0
-    for locus in sorted(segments):
-        segment = segments[locus]
-        number_of_matches_after_locus += segment[ITEM_SEGMENT_BEGIN] - segment[ITEM_SEGMENT_END]
-        segment[ITEM_NUMBER_OF_MATCHES] = number_of_matches_after_locus
+    for bp in sorted(locations):
+        location = locations[bp]
+        number_of_matches_after_locus += location[ITEM_SEGMENT_BEGIN] - location[ITEM_SEGMENT_END]
+        location[ITEM_NUMBER_OF_MATCHES] = number_of_matches_after_locus
 
 def plot_x_y(x, y, average):
     plt.xlabel("locus [Mb]")
@@ -61,28 +61,28 @@ def plot_x_y(x, y, average):
     plt.plot(x, y)
 
 
-def plot_result(segments, chromosome):
+def plot_matches(locations, chromosome):
     x = []    
     y = []
     
-    previous_locus = 0
-    previous_result = 0
+    previous_bp = 0
+    previous_matches = 0
     
-    locus_scaling_factor = 1000000
+    bp_scaling_factor = 1000000
     total_no_of_samples = 0
     sum_of_samples = 0
     
-    for locus in sorted(segments):
-        if (previous_locus > 0):    
-            x.append(previous_locus / locus_scaling_factor)
-            y.append(previous_result)
-            x.append((locus - 1) / locus_scaling_factor)
-            y.append(previous_result)
-            no_of_samples = locus - previous_locus;
+    for bp in sorted(locations):
+        if (previous_bp > 0):    
+            x.append(previous_bp / bp_scaling_factor)
+            y.append(previous_matches)
+            x.append((bp - 1) / bp_scaling_factor)
+            y.append(previous_matches)
+            no_of_samples = bp - previous_bp;
             total_no_of_samples += no_of_samples
-            sum_of_samples += no_of_samples * previous_result
-        previous_locus = locus
-        previous_result = segments[locus][ITEM_NUMBER_OF_MATCHES]
+            sum_of_samples += no_of_samples * previous_matches
+        previous_bp = bp
+        previous_matches = locations[bp][ITEM_NUMBER_OF_MATCHES]
     
     plot_x_y(x, y, sum_of_samples / total_no_of_samples)
 
@@ -93,13 +93,13 @@ def print_name_list(name_list, header):
             print("   ", name)
 
 
-def print_result(segments, chromosome, print_names):
-    for locus in sorted(segments):
-        print (str(chromosome) + ";" + str(locus) + ";" + str(segments[locus][ITEM_NUMBER_OF_MATCHES]))
+def print_matches(locations, chromosome, print_names):
+    for bp in sorted(locations):
+        print (str(chromosome) + ";" + str(bp) + ";" + str(locations[bp][ITEM_NUMBER_OF_MATCHES]))
         if (print_names):
-            print_name_list(segments[locus][ITEM_SEGMENT_BEGIN_NAMES], "Begin of match:")
-            print_name_list(segments[locus][ITEM_SEGMENT_END_NAMES], "End of match:")
-            # print (segments[locus][ITEM_SEGMENT_BEGIN_NAMES], ";", segments[locus][ITEM_SEGMENT_END_NAMES])
+            print_name_list(locations[bp][ITEM_SEGMENT_BEGIN_NAMES], "Begin of match:")
+            print_name_list(locations[bp][ITEM_SEGMENT_END_NAMES], "End of match:")
+            # print (locations[bp][ITEM_SEGMENT_BEGIN_NAMES], ";", locations[bp][ITEM_SEGMENT_END_NAMES])
 
 
 
@@ -117,13 +117,13 @@ parser = argparse.ArgumentParser(description=description)
 parser.add_argument('--file', dest='csv_file_path')
 parser.add_argument('--chromosome', dest='required_chromosome')
 parser.add_argument('--cm-limit', dest='cm_limit')
-parser.add_argument('--plot', dest='plot_result', action='store_true')
+parser.add_argument('--plot', dest='plot_matches', action='store_true')
 parser.add_argument('--print-names', dest='print_names', action='store_true')
 parser.add_argument('--save-picture', dest='save_picture')
 
 args = parser.parse_args()
 
-if (args.print_names and args.plot_result):
+if (args.print_names and args.plot_matches):
     print("Conflicting arguments --plot and --print-names", file=sys.stderr)
     exit() 
 
@@ -133,7 +133,7 @@ if (args.cm_limit):
 
 
 required_chromosome = int(args.required_chromosome)
-segments = dict()
+locations = dict()
 
 if (args.csv_file_path):
     print("reading csv file", args.csv_file_path, file=sys.stderr)
@@ -144,17 +144,17 @@ if (args.csv_file_path):
             if (header):
                 header = False
                 continue
-            register_segment(row, required_chromosome, cm_limit, segments)
+            register_segment(row, required_chromosome, cm_limit, locations)
 
-calculate_number_of_matches(segments)
+calculate_number_of_matches(locations)
 
-if (args.plot_result or args.save_picture):
-    plot_result(segments, required_chromosome)
+if (args.plot_matches or args.save_picture):
+    plot_matches(locations, required_chromosome)
     if (args.save_picture):
         plt.savefig(args.save_picture)
     else:
         plt.show()
 else:
-    print_result(segments, required_chromosome, args.print_names)
+    print_matches(locations, required_chromosome, args.print_names)
 
 
